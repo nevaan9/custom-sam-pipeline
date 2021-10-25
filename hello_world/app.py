e9c2ec1ccd8b24771fb259642967621520f02dfa
Nevaan Perera <nevaan9@gmail.com>
2021-10-25 16:38:33 -0400

    app change 4

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world, from the fucking pipeline 3!!",
+            "message": "hello world, from the fucking pipeline 4!!",
             # "location": ip.text.replace("\n", "")
         }),
     }
